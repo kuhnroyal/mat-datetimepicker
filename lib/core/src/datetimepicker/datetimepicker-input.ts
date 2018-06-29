5c72f1f393f7960d0612c317bd0efffea20a7790
Miroslav Dzurik <dzurik@bb.microcomp.sk>
2018-06-29 08:09:16 +0200

    Min/max time validation on type

--- lib/core/src/datetimepicker/datetimepicker-input.ts
+++ lib/core/src/datetimepicker/datetimepicker-input.ts
@@ -247,7 +247,7 @@ export class MatDatetimepickerInput<D> implements AfterContentInit, ControlValue
   private _minValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
     const controlValue = this._dateAdapter.getValidDateOrNull(this._dateAdapter.deserialize(control.value));
     return (!this.min || !controlValue ||
-      this._dateAdapter.compareDate(this.min, controlValue) <= 0) ?
+      this._dateAdapter.compareDatetime(this.min, controlValue) <= 0) ?
       null : {"matDatepickerMin": {"min": this.min, "actual": controlValue}};
   }
 
@@ -255,7 +255,7 @@ export class MatDatetimepickerInput<D> implements AfterContentInit, ControlValue
   private _maxValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
     const controlValue = this._dateAdapter.getValidDateOrNull(this._dateAdapter.deserialize(control.value));
     return (!this.max || !controlValue ||
-      this._dateAdapter.compareDate(this.max, controlValue) >= 0) ?
+      this._dateAdapter.compareDatetime(this.max, controlValue) >= 0) ?
       null : {"matDatepickerMax": {"max": this.max, "actual": controlValue}};
   }
 
