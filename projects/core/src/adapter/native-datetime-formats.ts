070d00cb0dd2064b5d1a3fd0756c4ede03291bc2
Peter Leibiger <kuhnroyal@gmail.com>
2018-07-09 14:21:39 +0200

    feat(format): default to 2-digit month/day/hour/second input and display formats Until now this has been mixed, now it is always 2-digit. Provide a custom MAT_DATETIME_FORMATS instance to overwrite this.
    Signed-off-by: Peter Leibiger <kuhnroyal@gmail.com>

--- projects/core/src/adapter/native-datetime-formats.ts
+++ projects/core/src/adapter/native-datetime-formats.ts
@@ -3,10 +3,10 @@ import { MatDatetimeFormats } from "./datetime-formats";
 export const MAT_NATIVE_DATETIME_FORMATS: MatDatetimeFormats = {
   parse: {},
   display: {
-    dateInput: {year: "numeric", month: "numeric", day: "numeric"},
+    dateInput: {year: "numeric", month: "2-digit", day: "2-digit"},
     monthInput: {month: "long"},
-    datetimeInput: {year: "numeric", month: "numeric", day: "numeric", hour: "numeric", minute: "numeric"},
-    timeInput: {hour: "numeric", minute: "numeric"},
+    datetimeInput: {year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit"},
+    timeInput: {hour: "2-digit", minute: "2-digit"},
     monthYearLabel: {year: "numeric", month: "short"},
     dateA11yLabel: {year: "numeric", month: "long", day: "numeric"},
     monthYearA11yLabel: {year: "numeric", month: "long"}
--- projects/moment/src/adapter/moment-datetime-formats.ts
+++ projects/moment/src/adapter/moment-datetime-formats.ts
@@ -2,13 +2,13 @@ import { MatDatetimeFormats } from "@mat-datetimepicker/core";
 
 export const MAT_MOMENT_DATETIME_FORMATS: MatDatetimeFormats = {
   parse: {
-    dateInput: "l",
+    dateInput: "L",
     monthInput: "MMMM",
     timeInput: "LT",
     datetimeInput: "L LT"
   },
   display: {
-    dateInput: "l",
+    dateInput: "L",
     monthInput: "MMMM",
     datetimeInput: "L LT",
     timeInput: "LT",
