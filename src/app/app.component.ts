9191a6167ba09340d5b4a6830ffbd3eb08aab432
Peter Leibiger <kuhnroyal@gmail.com>
2018-07-09 14:03:50 +0200

    fix(#23): placeholder not working with OnPush change detection
    * add MAT_INPUT_VALUE_ACCESSOR to MatDatetimepickerInput
    * use _formatValue to set native value instead of Renderer2
    Both changes are taken from the material2 source for MatDatepicker.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component
+} from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = "app";
