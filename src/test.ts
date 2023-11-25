ab7202734a65ff84fc4c38d80cd1b54a356e1eb6
Tweniee <abhisheku361@gmail.com>
2023-11-25 18:00:46 +0530

    chore: #503 V17 update

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,6 @@
 // This file is required by karma.conf.js: It loads all the .spec and framework files recursively.
 
-import 'zone.js/dist/zone-testing';
+import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
